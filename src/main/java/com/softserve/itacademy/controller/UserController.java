c12eae83d9a94a55c0cdca15efe68ac2a043ffab
Augustin Gerasym <augustingerasym@gmail.com>
2024-11-22 23:55:09 +0100

    Implemented getAll controller method.

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -71,8 +73,10 @@ public class UserController {
 //        // TODO
 //    }
 //
-//    @GetMapping("/all")
-//    public String getAll(/*add needed parameters*/) {
-//        // TODO
-//    }
+    @GetMapping("/all")
+    public String getAll(Model model) {
+        List<UserDto> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "users-list";
+    }
 }
