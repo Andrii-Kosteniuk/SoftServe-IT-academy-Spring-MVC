51c5e24b86402af46b40346dfa3736e9f3e3ce94
Augustin Gerasym <augustingerasym@gmail.com>
2024-11-23 02:25:12 +0100

    Implemented passing and displaying the error on the not-found.html page.

--- src/main/java/com/softserve/itacademy/config/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.itacademy.config.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
